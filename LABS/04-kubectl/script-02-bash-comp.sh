29292fa9bc64c5ecd7575734c58a9c049f9fc106
aminelli <antonio.minelli@etlforma.com>
2025-10-23 12:16:18 +0200

    upd

---
+++ LABS/04-kubectl/script-02-bash-comp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#####  KUBECTL AUTO COMPLETITION #####  
+# https://kubernetes.io/docs/reference/kubectl/generated/kubectl_completion/
+sudo apt install -y bash-completion
+
+# Installing bash completion on Linux
+## If bash-completion is not installed on Linux, install the 'bash-completion' package
+## via your distribution's package manager.
+## Load the kubectl completion code for bash into the current shell
+source <(kubectl completion bash)
+## Write bash completion code to a file and source it from .bash_profile
+kubectl completion bash > ~/.kube/completion.bash.inc
+printf "
+# kubectl shell completion
+source '$HOME/.kube/completion.bash.inc'
+" >> $HOME/.bash_profile
+source $HOME/.bash_profile
