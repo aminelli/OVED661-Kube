e3d6a40f557f52c7c56d09b4e4fbc2d4b465b635
aminelli <antonio.minelli@etlforma.com>
2025-10-23 14:40:31 +0200

    upd

--- LABS/05-tools/script-02-k9s.sh
+++ LABS/05-tools/script-02-k9s.sh
@@ -18,3 +18,4 @@ mkdir $HOME/downloads
 cd $HOME/downloads
 wget https://github.com/derailed/k9s/releases/latest/download/k9s_linux_amd64.deb && sudo apt install ./k9s_linux_amd64.deb && sudo rm k9s_linux_amd64.deb
 
+
